99ee7dcdd1b6db4c6b9f52c50c4f99ef9d0b0cc1
현요환 <hyeon-yohwan@hyeon-yohwan-ui-MacBookPro.local>
2021-03-09 16:45:40 +0900

    show video list with title

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAOBTz7AGMPumk6B5c5N86rk_N0kyRQHLk",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
+}
+
+export default App;
