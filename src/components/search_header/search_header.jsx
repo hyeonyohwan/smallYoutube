a7fb6906d5d5dd85c885e638cced0852a5baec5d
hyeonyohwan <hyunyohwan96@gmail.com>
2021-03-31 03:41:55 +0900

    modify errors

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,6 +1,7 @@
 import styles from "./search_header.module.css";
 import React, { memo, useRef } from "react";
-
+import Image1 from "../../images/logo.png";
+import Image2 from "../../images/search.png";
 const SearchHeader = memo(({ onSearch }) => {
   const inputRef = useRef();
   const handleSearch = () => {
@@ -20,7 +21,7 @@ const SearchHeader = memo(({ onSearch }) => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <img className={styles.img} src={Image1} alt="logo" />
         <h1 className={styles.title}>Youtube</h1>
       </div>
       <input
@@ -31,11 +32,7 @@ const SearchHeader = memo(({ onSearch }) => {
         onKeyPress={onKeyPress}
       />
       <button className={styles.button} type="submit" onClick={onClick}>
-        <img
-          className={styles.buttonImg}
-          src="/images/search.png"
-          alt="search"
-        />
+        <img className={styles.buttonImg} src={Image2} alt="search" />
       </button>
     </header>
   );
